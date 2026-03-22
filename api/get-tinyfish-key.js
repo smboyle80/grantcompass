ff53335bebb2e065890738b001ef64feeca96c39
smboyle80 <smdavi49@asu.edu>
2026-03-21 20:58:32 -0700

    folder adjustments

--- api/get-tinyfish-key.js
+++
@@ -1,8 +0,0 @@
-export default async function handler(req, res) {
-  if (req.method !== 'GET') return res.status(405).json({ error: 'Method Not Allowed' });
-
-  const key = process.env.TINYFISH_API_KEY;
-  if (!key) return res.status(500).json({ error: 'TINYFISH_API_KEY not configured' });
-
-  return res.status(200).json({ key });
-}
